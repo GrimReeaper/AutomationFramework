58c4c0893098fc5b7b5000ab2fe30e05ea63e46a
Shivam <shivamnagpal67@gmail.com>
2022-09-15 17:22:09 +0530

    Executed dataProvide thru Json file

--- src/test/java/Automation/SubmitOrderTest.java
+++ src/test/java/Automation/SubmitOrderTest.java
@@ -1,9 +1,16 @@
 package Automation;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Automation.TestComponents.BaseTest;
@@ -17,18 +24,18 @@ public class SubmitOrderTest extends BaseTest {
 	String ProductName="ZARA COAT 3";
 
      
-    @Test
-	public  void SubmitOrder() throws Exception {
+    @Test(dataProvider="getData", groups= {"Purchase"})
+	public  void SubmitOrder(HashMap<String, String> input) throws Exception {
 
 		String CountryName="india";
 		
-		ProductCatalog productCatalog=landingPage.loginApplication("shivam1@gmail.com", "Grimreaper1@");
+		ProductCatalog productCatalog=landingPage.loginApplication(input.get("email"), input.get("password"));
 		
 		List<WebElement> products=productCatalog.getProductList();
-		productCatalog.addProductToCart(ProductName);
+		productCatalog.addProductToCart(input.get("product"));
 		CartPage cartPage=productCatalog.goToCartPage();
 		
-		Boolean match=cartPage.verifyProductDisplay(ProductName);
+		Boolean match=cartPage.verifyProductDisplay(input.get("product"));
 		Assert.assertTrue(match);
 		CheckOutPage checkOutPage=cartPage.goToCheckOut();
 		
@@ -49,6 +56,27 @@ public class SubmitOrderTest extends BaseTest {
 		OrderPage orderPage=productCatalog.goToOrdersPage();
 		Assert.assertTrue(orderPage.verifyOrderDisplay(ProductName));
 
+    }
+    
+    public String getScreenshot(String testCaseName) throws IOException
+    {
+    	TakesScreenshot ts= (TakesScreenshot) driver;
+    	File source=ts.getScreenshotAs(OutputType.FILE);
+    	File file=new File(System.getProperty("user.dir")+"//reports//"+testCaseName+".png");
+    	FileUtils.copyFile(source, file);  
+    	return System.getProperty("user.dir")+"//reports//"+testCaseName+".png";
+    }
+    
+    
+    @DataProvider
+    public Object[][] getData() throws IOException
+    {
+
+    	
+    	List<HashMap<String, String>> data=getJsonDataToMap(System.getProperty("user.dir")+"\\src\\test\\java\\Data\\PurchaseOrder.json");
+    	return new Object[][] {{data.get(0)},{data.get(1)}};
+    	
+    	
     }
 
 }
--- src/test/java/Automation/TestComponents/BaseTest.java
+++ src/test/java/Automation/TestComponents/BaseTest.java
@@ -1,14 +1,22 @@
 package Automation.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObjects.LandingPage;
@@ -37,6 +45,16 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		return driver;
 		
+	}
+	
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException
+	{
+		String jsonContent=FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+		
+		ObjectMapper mapper=new ObjectMapper();
+		List<HashMap<String, String>> data= mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>()
+				{});
+		return data;
 	}
 		
 	    @BeforeMethod(alwaysRun=true)
